93ccac5f8b475f507ed37df1cb896e7ccd1d8ac8
Diane Yang <dianeyang0@gmail.com>
2014-07-25 20:13:18 -0400

    mocked up home page

---
+++ public/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$("body").css("display", "none");
+ 
+    $("body").fadeIn(1000);
+
+	//$('#i-am-a').waypoint(function() {
+	  //alert('Basic example callback triggered.');
+	//});
+});
