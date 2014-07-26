e545a1db9fa202931487b98e1e1e3259906d058f
Diane Yang <dianeyang0@gmail.com>
2014-07-26 10:05:53 -0400

    got routing working; added isotope as angular directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,46 @@
 'use strict';
 
-var app = angular.module('portfolio', [
-  'ngRoute', 'appControllers'
-]);
+function filterProjects(filter) {
+  $('#filters ul li a').removeClass('selected');
+  $('#filters ul li #' + filter).addClass('selected');
 
-app.config(['$routeProvider',
+  if(filter === 'all') {
+    filter = '*';
+  } else {
+    filter = '.' + filter
+  }
+
+  $('#projects ul').isotope({filter: filter});
+}
+
+var App = angular.module('portfolio', ['ngRoute']);
+
+App.config(['$routeProvider',
   function($routeProvider) {
-    $routeProvider.
-      when('/about', {
+    $routeProvider
+      .when('/about', {
         templateUrl: 'about.html',
-        controller: 'aboutController'
+      })
+      .when('/resume', {
+        templateUrl: 'resume.html',
+      })
+      .when('/contact', {
+        templateUrl: 'contact.html',
+      })
+      .when('/', {
+       templateUrl : 'work.html',
       });
   }]);
 
-var appControllers = angular.module('appControllers', []);
+App.directive('isotopeFiltering', function() {
+    return {
+        link: function() {
+          filterProjects('all');
 
-appControllers.controller('aboutController', ['$scope',
-  function($scope) {
-    $scope.message = 'LALALA'
-  }]);
+          $('#filters').on('click', 'ul li', function() {
+            var filter = $(this).find('a').attr('id');
+            filterProjects(filter);
+          });
+        }
+    };
+});
