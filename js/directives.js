53a49ada49142a14d1ca58c466ca0d0c2341fe44
Diane Yang <dianeyang0@gmail.com>
2014-08-03 11:00:14 -0400

    css tweaks

--- js/directives.js
+++ js/directives.js
@@ -65,3 +65,18 @@ appDirectives.directive('fadeFromTop', function() {
   }
 });
 
+appDirectives.directive('scrollTop', function() {
+  return {
+    scope: {
+      scroll: '=scrollPosition'
+    },
+    link: function(scope, element, attrs) {
+      var windowEl = angular.element($window);
+      var handler = function() {
+        scope.scroll = windowEl.scrollTop();
+      }
+      windowEl.on('scroll', scope.$apply.bind(scope, handler));
+      handler();
+    }
+  };
+})
