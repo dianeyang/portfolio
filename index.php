19b2a0a4543a8707f8ec53a5e963b9d23cf0c986
Diane Yang <dianeyang0@gmail.com>
2014-08-03 23:25:19 -0400

    lawl accidentaly characters

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900|Raleway:400,900' rel='stylesheet' type='text/css'>
 	        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
-xs
+
 	        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	        <script src="js/angular.js"></script>
 	        <script src="js/angular-route.js"></script>
